303e8bb1b6f64e5d1c50946e719d8ef092ff48cd
unknown <n.andyvue@outlook.com>
2022-09-20 17:24:15 -0500

    setup public and utils

--- server.js
+++ server.js
@@ -9,11 +9,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/social-network-API',
+  {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 mongoose.set('debug', true);
 
